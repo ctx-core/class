65c0183d29975b6214f4fc58651182b6ef8db530
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:54:42 -0400

    ## 5.2.1
    ### Patch Changes
    
    - .gitignore: - \*.js

---
+++ src/mixin.js
@@ -0,0 +1,11 @@
+export function mixin(target, source) {
+    const target_prototype = target.prototype;
+    const source_prototype = source.prototype;
+    const propertyNames = Object.getOwnPropertyNames(source_prototype);
+    for (let i = 0; i < propertyNames.length; i++) {
+        const propertyName = propertyNames[i];
+        if (propertyName !== 'constructor')
+            Object.defineProperty(target_prototype, propertyName, Object.getOwnPropertyDescriptor(source_prototype, propertyName));
+    }
+    return target;
+}
